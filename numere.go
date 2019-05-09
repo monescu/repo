42e069db45fa9e5e63ecaac2cca64ef406048e7f
elenabizoistudent <elena_ungur_89@yahoo.com>
2019-05-09 14:05:43 +0300

    ecuatia de gradul 2

---
+++ numere.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"math"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	tmpl := template.Must(template.ParseFiles("C:\\Users\\user\\go\\src\\numere\\index.html"))
+
+	http.HandleFunc("/", func  (w http.ResponseWriter, r *http.Request){
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		a1, _ := strconv.ParseFloat(r.FormValue("a"), 64)
+		b1, _ := strconv.ParseFloat(r.FormValue("b"), 64)
+		c1, _ := strconv.ParseFloat(r.FormValue("c"), 64)
+
+		sol1:= ((b1 * -1) - math.Sqrt((math.Pow(b1, 2))-(4*a1*c1)))/(2*a1)
+		sol2:= ((b1 * -1) + math.Sqrt((math.Pow(b1, 2))-(4*a1*c1)))/(2*a1)
+
+		fmt.Fprintf(w,"Solutiile ecuatiei: %f , %f.", sol1, sol2)
+
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
